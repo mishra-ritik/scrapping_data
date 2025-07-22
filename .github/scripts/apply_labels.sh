46f7924a428761e6e2f219b486473c6f33e52efa
Raunak <ritik.mishra@pal.tech>
2025-07-22 13:06:27 +0530

    Modularize the code

--- .github/scripts/apply_labels.sh
+++ .github/scripts/apply_labels.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 LABEL_NAME=$1
 
-curl -s -X POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels" \
+curl -s -X POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${{github.event.pull_request.number}}/labels" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "Content-Type: application/json" \
   -d "$(jq -n --arg label "$LABEL_NAME" '[$label]')"
