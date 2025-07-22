4485c352bc8b9ffcb7d11461d38d8bfddc7a4f76
Raunak <ritik.mishra@pal.tech>
2025-07-22 13:18:49 +0530

    Modularize the code

--- .github/scripts/manage_labels.sh
+++ .github/scripts/manage_labels.sh
@@ -5,12 +5,6 @@ LABEL_COLOR=$2
 
 LABEL_API_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/labels/$LABEL_NAME"
 
-# Try PATCH first, if fails, use POST
-curl -s -X PATCH "$LABEL_API_URL" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  -H "Content-Type: application/json" \
-  -d "$(jq -n --arg name "$LABEL_NAME" --arg color "$LABEL_COLOR" '{name: $name, color: $color}')" \
-|| \
 curl -s -X POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/labels" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "Content-Type: application/json" \
