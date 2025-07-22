38e8f4ab0987f496a779510ffe0b8a1d37c757fb
Raunak <ritik.mishra@pal.tech>
2025-07-22 17:54:04 +0530

    Modularize the code

--- .github/scripts/manage_labels.sh
+++ .github/scripts/manage_labels.sh
@@ -10,8 +10,3 @@ curl -s -X POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/labels" \
   -H "Content-Type: application/json" \
   -d "$(jq -n --arg name "$LABEL_NAME" --arg color "$LABEL_COLOR" '{name: $name, color: $color}')"
 
-# pwd
-# ls -al
-echo "Hello...."
-
-# bash ./.github/scripts/remove_labels.sh sonarqube_failed sonarqube_passed
--- .github/scripts/remove_labels_by_prefix.sh
+++ .github/scripts/remove_labels_by_prefix.sh
@@ -5,19 +5,6 @@ set -e
 PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 PREFIX=$1
 
-#if [[ -z "$PREFIX" ]]; then
-#  echo "ERROR: Prefix argument is missing"
-#  exit 1
-#fi
-#
-#if [[ -z "$GITHUB_TOKEN" || -z "$GITHUB_REPOSITORY" ]]; then
-#  echo "ERROR: Required environment variables not set"
-#  exit 1
-#fi
-#
-#echo "Found PR number: $PR_NUMBER"
-#echo "Repository: $GITHUB_REPOSITORY"
-
 LABELS=$(curl -s -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels" \
   | jq -r '.[].name')
