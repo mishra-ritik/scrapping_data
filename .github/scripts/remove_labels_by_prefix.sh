65c0f9a7e5cd7255cbdd9422b8645828584c41bd
Raunak <ritik.mishra@pal.tech>
2025-07-22 17:09:40 +0530

    fix: syntax error

--- .github/scripts/remove_labels_by_prefix.sh
+++ .github/scripts/remove_labels_by_prefix.sh
@@ -30,9 +30,5 @@ for label in $LABELS; do
   echo "Checking label: '$label'""
   if [[ "$label" == "$PREFIX"* ]]; then
     echo "Removing label: $label"
-    #ENCODED_LABEL=$(echo "$label" | jq -sRr @uri)
-    #curl -s -X DELETE \
-    #  "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels/${ENCODED_LABEL}" \
-    #  -H "Authorization: Bearer ${GITHUB_TOKEN}"
   fi
 done
