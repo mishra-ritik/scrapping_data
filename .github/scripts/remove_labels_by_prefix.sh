234279ecb30edaa179eeec615e061f903c19f149
Raunak <ritik.mishra@pal.tech>
2025-07-22 17:12:12 +0530

    test: substring

--- .github/scripts/remove_labels_by_prefix.sh
+++ .github/scripts/remove_labels_by_prefix.sh
@@ -28,7 +28,7 @@ echo "Looking for labels with prefix: $PREFIX:"
 IFS=$'\n' # Safely split by lines
 for label in $LABELS; do
   echo "Checking label: '$label'""
-  if [[ "$label" == "$PREFIX"* ]]; then
-    echo "Removing label: $label"
+  if [[ "$label" == *"$PREFIX"* ]]; then
+    echo "Removing label:"
   fi
 done
