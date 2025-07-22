c388681ba8dc59918c82230a43e58a6ce3b1b071
Raunak <ritik.mishra@pal.tech>
2025-07-22 17:25:49 +0530

    Modularize the code

--- .github/scripts/remove_labels_by_prefix.sh
+++ .github/scripts/remove_labels_by_prefix.sh
@@ -30,6 +30,8 @@ for label in $LABELS; do
   echo "Checking label: '$label'"
   if [[ "$label" == *"$PREFIX"* ]]; then
     echo "Removing label: $label and $PREFIX--)"
+    curl -s -X DELETE "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels/$label" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" || true
   else
     echo "Not Removing $label and $PREFIX--)"
   fi
